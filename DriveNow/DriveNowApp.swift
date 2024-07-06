87a9605a1a623ab6255206a6c776180dc82b2e9e
notFazal <fquadri224@gmail.com>
2024-07-06 14:46:18 -0500

    Initial Commit

---
+++ DriveNow/DriveNowApp.swift
@@ -0,0 +1,17 @@
+//
+//  DriveNowApp.swift
+//  DriveNow
+//
+//  Created by Hafsah Shamshuddin on 7/6/24.
+//
+
+import SwiftUI
+
+@main
+struct DriveNowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
